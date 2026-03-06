db8f0aaebbc9d3fb9c46581bf458f5836be57e76
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-03-06 20:39:01 +0100

    Implementando el método delete y creando un custom pipe ParseMongoIdPipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -14,6 +15,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
     PokemonModule,
+
+    CommonModule,
   ],
 })
 export class AppModule {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -76,8 +76,16 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    // const pokemon = await this.findOne( id );
+    // await pokemon.deleteOne();
+    // return { id };
+    // const result = await this.pokemonModel.findByIdAndDelete( id );
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+
+    return;
   }
 
   private handleExceptions(error) {
